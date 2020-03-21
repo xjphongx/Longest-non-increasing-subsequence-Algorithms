72bd74efc6deea8ba03ac14b666822e88ddc258b
xjphongx <JimmyPhong16@csu.fullerton.edu>
2020-03-20 21:23:37 -0700

    figured out the segmentation error

--- subsequence.hpp
+++ subsequence.hpp
@@ -56,42 +56,49 @@ bool is_nonincreasing(const sequence& A) {
   //TO DO: Write code for this function, including rewriting the return
   // statement, and then delete these comments.
   /*
-    Example: 6 5 7 4 1 
+    Example: 6 5 7 4 1
+          i= 0 1 2 3 4
+          size= 5
   */
-    for(int i = 0; i < A.size()+1; i++)
+ /*
+    for(int i = 0; i < A.size()-1; i++)
     {
-        if(A.at(i)<A.at(i+1))
+        if(A[i]< A[i+1])
         {
             return false; //return false if 5 < 7
-            break;//break out if there is a case
+            //break;//break out if there is a case
         }
     }
     return true; //return true if the sequence IS in nonincreasing order like 6 5 4 3 2 1
-
+    */
+   std::cout<<"hello";
+   return false;
   }
 
 sequence longest_nonincreasing_end_to_beginning(const sequence& A) {
-
-  const size_t n = A.size();
+  
+  const size_t n = A.size(); 
 
   // populate the array H with 0 values
-  std::vector<size_t> H(n, 0);
+  std::vector<size_t> H(n, 0); //step 1
 
   // calculate the values of array H
   // note that i has to be declared signed, to avoid an infinite loop, since
   // the loop condition is i >= 0
-  for (signed int i = n-2;  i>= 0; i--) {
-    for (size_t j = i+1; j < n ; j++) {
+  for (signed int i = n-1;  i>= 0; i--) { //step 2
+    for (size_t j = i+1; j < n-1; j++) { //step 3
       // TODO: write the statements that compute the value of
       // H[i] based on conditions that involve A[i], A[j] and H[j]
-      if (A[i] >= A[j]){ // if value of index i >= value of index j in A
+      
+      if (A[i] >= A[j]){ //step 4
         if(H[i] <= H[j]){
-          H[i] = H[j]+1;
+          //step 5
+          H[i] = H[j]+1; 
         }
       }
     }
   }
-
+  //std::cout<< "Exiting first loop\n";
   // calculate in max the length of the longest non-increasing subsequence
   // by adding 1 to the maximum value in H
   auto max = *std::max_element(H.begin(), H.end()) + 1;
@@ -102,18 +109,18 @@ sequence longest_nonincreasing_end_to_beginning(const sequence& A) {
   // add elements to R in non-increasing order whose H's values are
   // in decreasing order, starting with the element whose H value is max-1
   // store in index the H values sought: max-1, max-2, max-3, .. 0
-
+  //std::cout << "Entering second loop\n";
   size_t index = max-1, j = 0;
   for (size_t i = 0; i < n; ++i) {
-    if (H[i] == index) {
+    if (H[i] >= index) {
       // TODO: write the statements to add A[i] to the
       // sequence R by storing it into R[j], decrement
       // index and increment j
-      //R.push_back(j); // added this line to push something back into vector R
-      R[j]=A[i];
-      i--;
-      j++;
 
+    
+      R.at(j)=A.at(i);
+      j++;
+  
 
     }
   }
